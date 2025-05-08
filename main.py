90ff6ce87c534e7fb9c7c5eb23236e6954d68a64
J1MMYBR <joaopedroqmedeiros1006@gmail.com>
2025-05-08 09:17:17 -0300

    added a main.py

---
+++ main.py
@@ -0,0 +1,12 @@
+import dbManipulation
+import genDashboard
+
+def main():
+    dbManipulation.main()
+
+    genDashboard.main()
+
+    print("\n+++++ Programa finalizado com sucesso! +++++")
+
+if __name__ == "__main__":
+    main()
